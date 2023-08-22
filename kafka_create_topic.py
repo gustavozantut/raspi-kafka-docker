077b4c1c9b11797f633e4bb973c0dfd4432d481e
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-08-21 21:25:37 -0300

    change ip

--- kafka_create_topic.py
+++ kafka_create_topic.py
@@ -1,7 +1,7 @@
 from confluent_kafka.admin import AdminClient, NewTopic
 
 # Define the broker(s) you want to connect to
-bootstrap_servers = ["192.168.0.101:9092","192.168.14.2:9092"]
+bootstrap_servers = "192.168.0.101:9092,192.168.14.2:9092"
 
 # Create an AdminClient instance
 admin_client = AdminClient({"bootstrap.servers": bootstrap_servers})
