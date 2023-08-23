d3c884b9a2523d05acd0407feafaa61c37681f13
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-08-23 20:54:02 -0300

    try to reach sensor befor create topic

--- kafka_create_publish_topic.py
+++ kafka_create_publish_topic.py
@@ -4,6 +4,15 @@ import datetime
 import time
 from confluent_kafka.admin import AdminClient, NewTopic
 
+dht_pin = 4
+# Function to read DHT11 sensor data
+def read_dht11_sensor():
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, dht_pin)
+    return humidity, temperature
+
+while not read_dht11_sensor():
+    print('trying_to_reach_sensor')
+    time.sleep(5)
 
 # Define the broker(s) you want to connect to
 bootstrap_servers = "192.168.0.101:9092,192.168.14.2:9092,192.168.14.2:9093"
@@ -33,12 +42,6 @@ new_topic = NewTopic(
 
 # Create the topic
 admin_client.create_topics([new_topic])
-
-dht_pin = 4
-# Function to read DHT11 sensor data
-def read_dht11_sensor():
-    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, dht_pin)
-    return humidity, temperature
 
 try:
     # Create Kafka producer
