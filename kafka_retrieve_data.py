18eb7c01d4223ba9916081e4e9838371d295eca6
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-08-23 21:56:55 -0300

    add 3rd node ip to consumer

--- kafka_retrieve_data.py
+++ kafka_retrieve_data.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 
-bootstrap_servers = "192.168.0.101:9092,192.168.14.2:9092"  # Replace with the IP address of your Kafka broker
+bootstrap_servers = "192.168.0.101:9092,192.168.14.2:9092,192.168.14.2:9093"  # Replace with the IP address of your Kafka broker
 kafka_topic = "sensors_dht11"  # Use the same topic name that you used in the producer script
 
 def consume_kafka_data():
