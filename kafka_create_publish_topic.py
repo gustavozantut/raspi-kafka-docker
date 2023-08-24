e6edecddf4b572a3b019ed4648547af34d3eb5db
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2023-08-23 21:24:00 -0300

    change acks

--- kafka_create_publish_topic.py
+++ kafka_create_publish_topic.py
@@ -49,7 +49,7 @@ admin_client.create_topics([new_topic])
 
 try:
     # Create Kafka producer
-    producer = KafkaProducer(bootstrap_servers=bootstrap_servers, acks='all')
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers, acks='0')
     print("producer created")
     while True:
         # Read DHT11 sensor data
