2111bbe427fbf60438206d91af676fc2b73b458f
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2024-03-15 10:33:42 -0300

    af

--- kafka_create_publish_topic.py
+++ kafka_create_publish_topic.py
@@ -47,9 +47,9 @@ try:
                 }
             
             try:
-                print("trying")
+            
                 # Publish the payload to the Kafka topic
-                producer.send("dht11", value=payload.encode('utf-8')).get()
+                print(producer.send("dht11", value=payload.encode('utf-8')).get())
                 print("Published:\n", payload)
             
             except:
